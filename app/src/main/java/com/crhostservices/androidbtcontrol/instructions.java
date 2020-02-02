40072f67b5bc34fb1a5258a56f8354a53856601e
mcoto004CR <crhostservices@gmail.com>
2020-02-01 19:05:21 -0600

    First commit

---
+++ app/src/main/java/com/crhostservices/androidbtcontrol/instructions.java
@@ -0,0 +1,14 @@
+package com.crhostservices.androidbtcontrol;
+
+import android.os.Bundle;
+
+/**
+ * Created by marcc on 7/10/2017.
+ */
+
+public class instructions extends MainActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructions);
+    }
+}
